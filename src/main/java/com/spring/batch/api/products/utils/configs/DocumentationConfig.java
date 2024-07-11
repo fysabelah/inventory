4ffd32590c623360e523f4eae9cdc108c5223c06
Isabela França <fysabelah@gmail.com>
2024-07-11 18:05:16 -0300

    Adicionar conexão com gateway

--- src/main/java/com/spring/batch/api/products/utils/configs/DocumentationConfig.java
+++ src/main/java/com/spring/batch/api/products/utils/configs/DocumentationConfig.java
@@ -2,11 +2,15 @@ package com.spring.batch.api.products.utils.configs;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DocumentationConfig {
+
+    @Value("${swagger.gateway.address}")
+    private String gateway;
 
     @Bean
     public OpenAPI apiDocConfig() {
@@ -14,7 +18,7 @@ public class DocumentationConfig {
                 .info(
                         new Info()
                                 .title("Gerenciamento de Estoque")
-                                .description("API para gerenciamento de produtos e atualização em batch")
+                                .description("API para gerenciamento e atualização em batch de produtos")
                                 .version("1.0.0")
                 );
     }

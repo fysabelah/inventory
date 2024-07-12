e11a4976bdcab04493fddf4835769de0d02570dc
Isabela França <fysabelah@gmail.com>
2024-07-12 18:19:53 -0300

    Atualizando para incluir no docker e gateway

--- src/main/java/com/spring/batch/api/products/utils/configs/DocumentationConfig.java
+++ src/main/java/com/spring/batch/api/products/utils/configs/DocumentationConfig.java
@@ -2,7 +2,6 @@ package com.spring.batch.api.products.utils.configs;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
